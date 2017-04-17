ce0c88cfd7726a4d0c2162538dcea41256745c5d
TSITCL\s162034 <elitesr2110@gmail.com>
2017-04-17 16:09:11 +0900

    プロジェクトの追加

---
+++ CueSheet_0.h
@@ -0,0 +1,26 @@
+/*===========================================================================*
+ *  Header file for Atom CueSheet Binary
+ *  Project          : test
+ *  Tool Ver.        : CRI Atom Craft LE Ver.2.13.00
+ *  File Path        : C:/Users/s162196/Documents/CriAtomCraft/test/Public/WorkUnit_0/CueSheet_0.acb
+ *  File Name        : CueSheet_0.acb
+ *  File Size        : 5,472 bytes
+ *  Date Time        : 2017/04/13 18:10
+ *  Target           : Public
+ *  Cues             : 1
+ *  CueSheet Comment : 
+ *===========================================================================*/
+
+#define CRI_CUESHEET_0_CUENUM (1)
+
+/* AISAC Control List (AISAC Control ID) */
+// No AISAC Control
+
+
+/* Cue List (Cue ID) */
+#define CRI_CUESHEET_0_BGM  ( 0) /*   */
+
+/* Block List (Block Index) */
+
+/* end of file */
+
